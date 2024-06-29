b44a7ad1ac99b63813e7f683eba0c158d8169369
Giftmasil <masilagift@gmail.com>
2024-06-29 20:43:42 +0300

    Update allowedOrigins.js

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "https://www.dandrepairshop.com",
     "https://mern-stark-full-tutorial.onrender.com/",
+    "https://mern-stark-full-tutorial.onrender.com",
     "http://dandrepairshop.com"
 ]
 
